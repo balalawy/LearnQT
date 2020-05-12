bb2722530ea704d7d85205c08aff4e824ba2be57
balalawy <47027345+balalawy@users.noreply.github.com>
2020-05-12 10:20:15 +0800

    完成了布局案例：Cards (使用自定义布局CardLayout完成的)

--- LayoutLearn/main.cpp
+++ LayoutLearn/main.cpp
@@ -4,6 +4,7 @@
 #include "GridLayout.h"
 #include "FormLayout.h"
 #include "AddressBook.h"
+#include "CardLayout.h"
 
 #include <QtWidgets/QApplication>
 
@@ -37,13 +38,37 @@ void layoutDemo_AddressBook()
 }
 
 
+// 布局案例: Cards
+void layoutDemo_Cards()
+{
+	QWidget* window = new QWidget;
+
+	QPushButton* button1 = new QPushButton("button1");
+	QPushButton* button2 = new QPushButton("button2");
+	QPushButton* button3 = new QPushButton("button3");
+
+	CardLayout* layout = new CardLayout(window);
+	layout->addWidget(button1);
+	layout->addWidget(button2);
+	layout->addWidget(button3);
+
+	//QRect rect(0, 0, 600, 600);
+	//layout->setGeometry(rect);
+	layout->setSpacing(0);  // set the layoutitem's spacing
+
+	window->show();
+}
+
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
 	// learnLayout();  // 学习布局
 
-	layoutDemo_AddressBook();  // 布局案例：AddressBook
+	// layoutDemo_AddressBook();  // 布局案例：AddressBook
+
+	layoutDemo_Cards();  // 布局案例: Cards
 
 	return a.exec();
 }
